9dc115dbf76c5ca74ee976e62651913063f9b393
Viktoria Zaharova <zaharova181114@gmail.com>
2025-10-13 13:48:48 +0300

    pc version all

--- js/common.js
+++ js/common.js
@@ -368,6 +368,44 @@ $('.similar-slider').slick({
   ]
 });
 
+$('.team-slider').slick({
+  slidesToShow: 3,
+  appendArrows: '.team-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: true
+      }
+    }
+    
+  ]
+});
+
+$('.certificates-slider').slick({
+  slidesToShow: 4,
+  appendArrows: '.certificates-slider__nav',
+  prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: true
+      }
+    }
+  ]
+});
 
 // map tooltip svg
 $(function() {
